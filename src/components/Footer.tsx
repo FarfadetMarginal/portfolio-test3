c8c543283225a9a1f1b4c3217079f095d886597c
FarfadetMarginal <bapt935@gmail.com>
2026-03-09 16:56:15 +0100

    first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+    return <footer> © 2025 Baptiste COUTAS - tous droits réservés - merci d'avoir lu jusqu'ici ☺</footer>;
+};
+
+export default Footer;
---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,17 @@
+type ProjectCardProps = {
+    title: string;
+    description: string;
+    image: string;
+    link: string;
+};
+
+const ProjectCard: React.FC<ProjectCardProps> = ({ title, description, image, link }) => (
+<div className="project-card">
+<img src={image} alt={title} />
+<h3>{title}</h3>
+<p>{description}</p>
+<a href={link}></a>
+</div>
+);
+
+export default ProjectCard;
